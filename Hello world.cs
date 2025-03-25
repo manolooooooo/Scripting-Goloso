84bb9bed5ff39b1b3212f62759abf0023cfd5efd
manolooooooo <alg.heredia.manu@gmail.com>
2025-03-25 18:15:56 +0100

    Sx

---
+++ Hello world.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Helloworld : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        print("Hi World"); //Escribe "Hi World" en la pantalla
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
